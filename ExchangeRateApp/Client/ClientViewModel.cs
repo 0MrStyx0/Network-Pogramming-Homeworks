1b408f5e9118c81f5c028ccc7a9d08c19bbc7b46
Nazim Dovlatov <159007876+0MrStyx0@users.noreply.github.com.>
2024-10-05 11:06:51 +0400

    ExchangeRateApp ver 1

---
+++ ExchangeRateApp/Client/ClientViewModel.cs
@@ -0,0 +1,175 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    public class ClientViewModel : ViewModelBase
+    {
+        private string ip;
+        public string IP
+        {
+            get { return ip; } set { Set(ref ip, value); }
+        }
+        private ObservableCollection<string> currency1;
+        public ObservableCollection<string> Currency1
+        {
+            get { return currency1; } set { Set(ref currency1, value); }
+        }
+
+        private ObservableCollection<string> currency2;
+        public ObservableCollection<string> Currency2
+        {
+            get { return currency2; }
+            set { Set(ref currency2, value); }
+        }
+        public string SelectedItem1 { get; set; }
+        public string SelectedItem2 { get; set; }
+
+        private string result;
+        public string Result
+        {
+            get { return result; } set { Set(ref result, value); }
+        }
+
+        private bool isLimiteReached = false;
+        private const int waitTimeSeconds = 15;
+        public ClientViewModel()
+        {
+            Currency1 = new ObservableCollection<string>() { "USD","RUB","AZN" };
+            Currency2 = new ObservableCollection<string>() { "USD","RUB","AZN" };
+            Client = new TcpClient();
+        }
+
+        public TcpClient Client {  get; set; }
+
+        private RelayCommand connect;
+        public RelayCommand Connect
+        {
+            get
+            {
+                return connect ?? new RelayCommand(() =>
+                {
+                    ConnectToServerAsync();
+                });
+            }
+        }
+
+
+        private RelayCommand disconnect;
+        public RelayCommand Disconnect
+        {
+            get
+            {
+                return disconnect ?? new RelayCommand(() =>
+                {
+                    DisconnectFromServerAsync();
+                });
+            }
+        }
+
+        private RelayCommand show;
+        public RelayCommand Show
+        {
+            get
+            {
+                return show ?? new RelayCommand(() =>
+                {
+                    CalculateRate();
+                });
+            }
+        }
+
+        private void CalculateRate()
+        {
+            if (Client.Connected)
+            {
+                if (SelectedItem1 == null || SelectedItem2 == null)
+                {
+                    MessageBox.Show("Значения не должны быть пустыми!");
+                }
+                else if (SelectedItem1 == SelectedItem2) MessageBox.Show("Конвертация Невозможна");
+                else
+                {
+                    try
+                    {
+                        NetworkStream networkstream = Client.GetStream();
+                        StreamWriter writer = new StreamWriter(networkstream) { AutoFlush = true };
+                        StreamReader reader = new StreamReader(networkstream);
+                        writer.WriteLine(SelectedItem1);
+                        writer.WriteLine(SelectedItem2);
+                        Result = reader.ReadLine();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Вы превысили лимит запросов и Сервер отключил вас. Повторная попытка через 15 секунд...");
+                        isLimiteReached = true;
+                        Client.Close();
+                    }
+
+                }
+            }
+            else MessageBox.Show("Клиент Не подключен!!!");
+        }
+        private async Task ConnectToServerAsync()
+        {
+            try
+            {
+                if (Client.Connected) throw new Exception("Клиент уже подключен");
+
+                if(isLimiteReached)
+                {
+                    MessageBox.Show("Вы превысили лимит запросов. Повторная попытка через 15 секунд...");
+                    await Task.Delay(waitTimeSeconds * 1000);
+                    isLimiteReached = false;
+                    return;
+                }
+
+                await Task.Delay(3000);
+                Client = new TcpClient();
+                await Client.ConnectAsync(IPEndPoint.Parse(IP));
+                NetworkStream networkstream = Client.GetStream();
+                StreamReader reader = new StreamReader(networkstream);
+                if(reader.ReadLine()== "Сервер Переполнен!!!")
+                {
+                    throw new Exception("Сервер Переполнен!!!");
+                }
+
+                MessageBox.Show("Клиент подключился к серверу");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                if(ex.Message == "Сервер Переполнен!!!") Client.Close();
+            }
+        }
+        private async Task DisconnectFromServerAsync()
+        {
+            try
+            {
+                if (Client.Connected)
+                {
+                    Client.Close();
+                    MessageBox.Show("Клиент отключился от сервера");
+                }
+                else
+                {
+                    MessageBox.Show("Клиент ни присоединен ни к одному серверу");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExchangeRateApp/Server/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+TcpListener listener = new TcpListener(IPEndPoint.Parse("127.0.0.1:8080"));
+const int MaxClients = 2;
+const int MaxRequestsPerClient = 1;
+listener.Start(MaxClients);
+Console.WriteLine($"{DateTime.Now.ToString()}: Server Started on {listener.LocalEndpoint}");
+
+List<Rate> Rates = new List<Rate>()
+{
+   new Rate {From = "USD" , To = "AZN" , Value = 1.7m},
+   new Rate {From = "USD" , To = "RUB" , Value = 95.1198m},
+   new Rate {From = "RUB" , To = "USD" , Value = 0.0105m},
+   new Rate {From = "RUB" , To = "AZN" , Value = 0.01785m},
+   new Rate {From = "AZN" , To = "USD" , Value = 0.58823m},
+   new Rate {From = "AZN" , To = "RUB" , Value = 55.9528m},
+};
+
+List<TcpClient> Clients = new List<TcpClient>();
+ 
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    NetworkStream networkstream = client.GetStream();
+    StreamReader reader = new StreamReader(networkstream);
+    StreamWriter writer = new StreamWriter(networkstream) { AutoFlush = true };
+    if (Clients.Count >= MaxClients)
+    {
+        writer.WriteLine("Сервер Переполнен!!!");
+        Console.WriteLine($"{DateTime.Now.ToLongTimeString()} {client.Client.RemoteEndPoint} отключился из-за переполнения сервера");
+        client.Close();
+    }
+    else
+    {
+        writer.WriteLine();
+        var clientInfo = new ClientInfo { Client = client, RequestCount = 0 };
+        Clients.Add(client);
+        RequestProcessingAsync(clientInfo, reader, writer);
+    }
+}
+
+ async Task RequestProcessingAsync(ClientInfo clientInfo, StreamReader reader, StreamWriter writer)
+{
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+    Task.Run(async() =>
+    {
+        Console.WriteLine($"{DateTime.Now.ToLongTimeString()} {clientInfo.Client.Client.RemoteEndPoint} connected to server");
+
+        while (true)
+        {
+            try
+            {
+                string currency1 = await reader.ReadLineAsync();
+                string currency2 = await reader.ReadLineAsync();
+                if (currency1 == null || currency2 == null) throw new Exception($"{DateTime.Now.ToLongTimeString()} {clientInfo.Client.Client.RemoteEndPoint} Disconnect!");
+
+                Console.WriteLine($"Rates to exchange: from {currency1} to {currency2}");
+
+                var item = Rates.Find(x => { if (x.From == currency1 && x.To == currency2) return true; return false; });
+
+                if (item != null)
+                {
+                    writer.WriteLine(item.Value.ToString());
+                }
+
+                clientInfo.RequestCount++;
+                if (clientInfo.RequestCount >= MaxRequestsPerClient) 
+                {
+                    writer.WriteLine($"Вы превысили лимит в {MaxRequestsPerClient} запросов. Соединение будет закрыто.");
+                    throw new Exception($"Вы превысили лимит в {MaxRequestsPerClient} запросов. Соединение будет закрыто.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                clientInfo.Client.Close();
+                reader.Close();
+                writer.Close();
+                Clients.Remove(clientInfo.Client);
+                break;
+            }
+        }
+    });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+}
+
+
+public class Rate
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public decimal Value { get; set; }
+}
+
+public class ClientInfo
+{
+    public TcpClient Client { get; set; }
+    public int RequestCount { get; set; }
+}
