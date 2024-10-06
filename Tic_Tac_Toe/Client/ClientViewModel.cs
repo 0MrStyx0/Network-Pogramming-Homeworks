3d3c80064a7add5614c43dc4ec7df78cf4e66c7f
Nazim Dovlatov <159007876+0MrStyx0@users.noreply.github.com.>
2024-10-06 17:35:04 +0400

    Tic_Tac_Toe Demo

---
+++ Tic_Tac_Toe/Client/ClientViewModel.cs
@@ -0,0 +1,189 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    public class ClientViewModel:ViewModelBase
+    {
+        string currentPlayer;
+        public string CurrentPlayer
+        {
+            get { return currentPlayer; }
+            set { Set(ref currentPlayer, value); }
+        }
+
+        string buttonSymb1;
+        public string ButtonSymb1
+        {
+            get { return buttonSymb1; } set { Set(ref buttonSymb1, value);}
+        }
+
+        string buttonSymb2;
+        public string ButtonSymb2
+        {
+            get { return buttonSymb2; }
+            set { Set(ref buttonSymb2, value); }
+        }
+
+        string buttonSymb3;
+        public string ButtonSymb3
+        {
+            get { return buttonSymb3; }
+            set { Set(ref buttonSymb3, value); }
+        }
+
+        string buttonSymb4;
+        public string ButtonSymb4
+        {
+            get { return buttonSymb4; }
+            set { Set(ref buttonSymb4, value); }
+        }
+
+        string buttonSymb5;
+        public string ButtonSymb5
+        {
+            get { return buttonSymb5; }
+            set { Set(ref buttonSymb5, value); }
+        }
+
+        string buttonSymb6;
+        public string ButtonSymb6
+        {
+            get { return buttonSymb6; }
+            set { Set(ref buttonSymb6, value); }
+        }
+
+        string buttonSymb7;
+        public string ButtonSymb7
+        {
+            get { return buttonSymb7; }
+            set { Set(ref buttonSymb7, value); }
+        }
+
+        string buttonSymb8;
+        public string ButtonSymb8
+        {
+            get { return buttonSymb8; }
+            set { Set(ref buttonSymb8, value); }
+        }
+
+        string buttonSymb9;
+        public string ButtonSymb9
+        {
+            get { return buttonSymb9; }
+            set { Set(ref buttonSymb9, value); }
+        }
+
+        bool isButtonPressed = false;
+
+        bool isConnectedToServer = false;
+
+        public TcpClient Client { get; set; }
+
+        public ClientViewModel()
+        {
+            checkBoard();
+        }
+
+        RelayCommand connect;
+        public RelayCommand Connect
+        {
+            get
+            {
+                return connect ?? new RelayCommand(() =>
+                {
+                    ConnectToServer();
+                });
+            }
+        }
+
+        RelayCommand<string> setSymb;
+        public RelayCommand<string> SetSymb
+        {
+            get
+            {
+                return setSymb ?? new RelayCommand<string>((button) =>
+                {
+                    SetSymbol(button);
+                });
+            }
+        }
+
+        void SetSymbol(string button)
+        {
+            if(isButtonPressed == false)
+            {
+                bool isDone = false;
+                if (button == "1" && ButtonSymb1 == null) { ButtonSymb1 = CurrentPlayer; isDone = true; } 
+                else if (button == "2" && ButtonSymb2 == null) { ButtonSymb2 = CurrentPlayer; isDone = true; }
+                else if (button == "3" && ButtonSymb3 == null) { ButtonSymb3 = CurrentPlayer; isDone = true; }
+                else if (button == "4" && ButtonSymb4 == null) { ButtonSymb4 = CurrentPlayer; isDone = true; }
+                else if (button == "5" && ButtonSymb5 == null) { ButtonSymb5 = CurrentPlayer; isDone = true; }
+                else if (button == "6" && ButtonSymb6 == null) { ButtonSymb6 = CurrentPlayer; isDone = true; }
+                else if (button == "7" && ButtonSymb7 == null) { ButtonSymb7 = CurrentPlayer; isDone = true; }
+                else if (button == "8" && ButtonSymb8 == null) { ButtonSymb8 = CurrentPlayer; isDone = true; }
+                else if (button == "9" && ButtonSymb9 == null) { ButtonSymb9 = CurrentPlayer; isDone = true; }
+
+                if (isDone == true)
+                {
+                    NetworkStream networkStream = Client.GetStream();
+                    StreamWriter writer = new StreamWriter(networkStream) { AutoFlush = true };
+                    writer.WriteLine(button);
+                    isButtonPressed = true;
+                }
+                else { }
+            }
+            else if (isButtonPressed == true) { }
+        }
+
+        async Task ConnectToServer()
+        {
+            Client = new TcpClient();
+            Client.ConnectAsync(IPEndPoint.Parse("127.0.0.1:8080"));
+            MessageBox.Show("Подключился");
+            isConnectedToServer = true;
+            NetworkStream networkStream = Client.GetStream();
+            StreamReader reader = new StreamReader(networkStream);
+            CurrentPlayer = reader.ReadLine();
+        }
+
+        async Task checkBoard()
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    if (isButtonPressed == false && isConnectedToServer == true)
+                    {
+                        NetworkStream networkStream = Client.GetStream();
+                        StreamReader reader = new StreamReader(networkStream);
+                        string symb = reader.ReadLine();
+                        string position = reader.ReadLine();
+                        if (position == "1") ButtonSymb1 = symb;
+                        else if (position == "2") ButtonSymb2 = symb;
+                        else if (position == "3") ButtonSymb3 = symb;
+                        else if (position == "4") ButtonSymb4 = symb;
+                        else if (position == "5") ButtonSymb5 = symb;
+                        else if (position == "6") ButtonSymb6 = symb;
+                        else if (position == "7") ButtonSymb7 = symb;
+                        else if (position == "8") ButtonSymb8 = symb;
+                        else if (position == "9") ButtonSymb9 = symb;
+
+                        isButtonPressed = false;
+                    }
+                }
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+    }
+}
---
+++ Tic_Tac_Toe/Server/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+TcpListener listener = new TcpListener(IPEndPoint.Parse("127.0.0.1:8080"));
+listener.Start();
+Console.WriteLine($"{DateTime.Now.ToString()}: Server Started on {listener.LocalEndpoint}");
+
+while (true)
+{
+    var client1 = await listener.AcceptTcpClientAsync();
+    NetworkStream networkStream1 = client1.GetStream();
+    StreamWriter writer1 = new StreamWriter(networkStream1) { AutoFlush = true };
+    string symb1 = "X";
+    writer1.WriteLine(symb1);
+
+    var client2 = await listener.AcceptTcpClientAsync();
+    NetworkStream networkStream2 = client2.GetStream();
+    StreamWriter writer2 = new StreamWriter(networkStream2) { AutoFlush = true };
+    string symb2 = "O";
+    writer2.WriteLine(symb2);
+    Handle(client1, writer1, symb1,client2, writer2, symb2);
+}
+
+async Task Handle(TcpClient client1, StreamWriter writer1, string symbX, TcpClient client2, StreamWriter writer2, string symbO)
+{
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+    Task.Run(async() =>
+    {
+        string[][] board = new string[3][];
+        board[0] = new string[3];
+        board[1] = new string[3];
+        board[2] = new string[3];
+        bool isXWin = false;
+        bool isOWin = false;
+        while (true)
+        {
+            StreamReader reader1 = new StreamReader(client1.GetStream());
+            string positionX = await reader1.ReadLineAsync();
+            if(positionX == "1") board[0][0] = symbX;
+            else if(positionX=="2") board[0][1] = symbX;
+            else if(positionX=="3") board[0][2] = symbX;
+            else if(positionX=="4") board[1][0] = symbX;
+            else if(positionX=="5") board[1][1] = symbX;
+            else if(positionX=="6") board[1][2] = symbX;
+            else if(positionX=="7") board[2][0] = symbX;
+            else if(positionX=="8") board[2][1] = symbX;
+            else if(positionX=="9") board[2][2] = symbX;
+
+            writer2.WriteLine(symbX);
+            writer2.WriteLine(positionX);
+
+            for (int i = 0; i < board.Length; i++) 
+            {
+                for (int j = 0; j < board[i].Length; j++) 
+                {
+                    if (j == 2) Console.WriteLine("|  " + board[i][j]);
+                    else Console.Write("|  " + board[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+
+
+            StreamReader reader2 = new StreamReader(client2.GetStream());
+            string positionO = await reader2.ReadLineAsync();
+            if (positionO == "1") board[0][0] = symbO;
+            else if (positionO == "2") board[0][1] = symbO;
+            else if (positionO == "3") board[0][2] = symbO;
+            else if (positionO == "4") board[1][0] = symbO;
+            else if (positionO == "5") board[1][1] = symbO;
+            else if (positionO == "6") board[1][2] = symbO;
+            else if (positionO == "7") board[2][0] = symbO;
+            else if (positionO == "8") board[2][1] = symbO;
+            else if (positionO == "9") board[2][2] = symbO;
+
+            writer1.WriteLine(symbO);
+            writer1.WriteLine(positionO);
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (j == 2) Console.WriteLine("|  " + board[i][j]);
+                    else Console.Write("|  " + board[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+
+            //CheckWin(board, isXWin, isOWin, writer1, writer2);
+        }
+    });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+}
+
+void CheckWin(string[][] board, bool isXWin, bool isOWin, StreamWriter writer1, StreamWriter writer2)
+{
+    for(int i = 0;i < board.Length; i++)
+    {
+        if (board[i][0] != null && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+        {
+            if(board[i][0] == "X") isXWin = true;
+            else if (board[i][0] == "O") isOWin = true;
+        }
+    }
+
+    for(int i = 0; i < board.Length; i++)
+    {
+        if (board[0][i]!=null &&board[0][i] == board[1][i] && board[1][i] == board[2][i])
+        {
+            if (board[0][i] == "X") isXWin = true;
+            else if (board[0][i] == "O") isOWin = true;
+        }
+    }
+
+    if (board[0][0]!=null &&board[0][0] == board[1][1] && board[1][1] == board[2][2]) 
+    {
+        if (board[0][0] == "X") isXWin = true;
+        else if (board[0][0] == "O") isOWin = true;
+    }
+    if (board[0][2] != null && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        if (board[0][2] == "X") isXWin = true;
+        else if (board[0][2] == "O") isOWin = true;
+    }
+
+    if(isXWin == true || isOWin == true)
+    {
+        writer1.WriteLine(isXWin);
+        writer2.WriteLine(isOWin);
+    }
+    else
+    {
+        writer1.WriteLine(" ");
+        writer2.WriteLine(" ");
+    }
+
+}
